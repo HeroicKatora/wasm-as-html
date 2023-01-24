fc0f5e304ba39956650f8d77b45e03470a45d559
Andreas Molzer <andreas.molzer@gmx.de>
2023-01-24 22:26:27 +0100

    PoC working version

--- wasi-loader/stage2-wasi.js
+++ wasi-loader/stage2-wasi.js
@@ -1,16 +1,25 @@
 import { WASI, File, PreopenDirectory } from "@bjorn3/browser_wasi_shim";
 
 async function mount(promise) {
-  let args = ["bin", "arg1", "arg2"];
+  let args = ["hq9+", "-f", "test.hq9+"];
   let env = ["FOO=bar"];
+
+  var stdin = new File([]);
+  var stdout = new File([]);
+  var stderr = new File([]);
+  
+  let dir = new PreopenDirectory(".", {
+      "stdin": stdin,
+      "stdout": stdout,
+      "stderr": stderr,
+      "test.hq9+": new File(new TextEncoder("utf-8").encode(`HHHH+Q`)),
+    });
+
   let fds = [
-    new File([]), // stdin
-    new File([]), // stdout
-    new File([]), // stderr
-    new PreopenDirectory(".", {
-      "example.c": new File(new TextEncoder("utf-8").encode(`#include "a"`)),
-      "hello.rs": new File(new TextEncoder("utf-8").encode(`fn main() { println!("Hello World!"); }`)),
-    }),
+    dir.path_open(0, "stdin", 0).fd_obj,
+    dir.path_open(0, "stdout", 0).fd_obj,
+    dir.path_open(0, "stderr", 0).fd_obj,
+    dir,
   ];
 
   let wasi = new WASI(args, env, fds);
@@ -20,7 +29,15 @@ async function mount(promise) {
     "wasi_snapshot_preview1": wasi.wasiImport,
   });  
 
-  wasi.start(inst);
+  try {
+    wasi.start(inst);
+  } finally {
+    let decoder = new TextDecoder();
+    console.log(stdin, stdout, stderr);
+    console.log(decoder.decode(stdin.data));
+    console.log(decoder.decode(stdout.data));
+    console.log(decoder.decode(stderr.data));
+  }
 }
 
 export default mount;
